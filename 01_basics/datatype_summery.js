04b2e78964f220da5f61620b4adecad463d30ecd
Jeeldobariya33 <24010101066@darshan.ac.in>
2025-11-21 22:05:17 +0530

    Add datatype summary with examples of primitive and non-primitive data types in JavaScript

---
+++ 01_basics/datatype_summery.js
@@ -0,0 +1,26 @@
+// datatype_summery.js
+
+// 2 types of data types in JavaScript
+// 1. Primitive Data Types
+// 2. Non-Primitive Data Types (Reference Data Types)
+
+// 1. Primitive Data Types
+// These data types store single values and are immutable.
+let num = 42; // Number
+let str = "Hello, World!";
+let bool = true; // Boolean
+let undef; // Undefined
+let nul = null; // Null
+let sym = Symbol("id"); // Symbol
+let bigInt = BigInt(9007199254740991); // BigInt
+
+c
+// 2. Non-Primitive Data Types (Reference Data Types)
+// These data types store references to objects and are mutable.
+let obj = { name: "Alice", age: 30 }; // Object
+let arr = [1, 2, 3, 4, 5]; // Array
+function greet() {
+  console.log("Hello!");
+} // Function
+
+console.log("--------------------------------------------------");
